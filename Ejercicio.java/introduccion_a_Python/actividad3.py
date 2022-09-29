5ee17db76039145eddc59b6163820b391287bed5
vanecente <vanecente@gmail.com>
2022-09-29 13:49:53 -0300

    Create actividad3.py

---
+++ Ejercicio.java/introduccion_a_Python/actividad3.py
@@ -0,0 +1,7 @@
+peso = int(input( 'Ingrese su peso en kg'))
+altura= float(input ( ' Ingrese su altura en metros'))
+
+masa_muscular = int(peso )/ float(altura*altura)
+
+print (f'Su masa muscular es de',(round(masa_muscular,2)))
+
